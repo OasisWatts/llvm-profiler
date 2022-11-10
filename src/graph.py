43bb93a72f143d004bc0cf8f70130f7e8c6d6bfc
OASISWatts <leahymj2@gmail.com>
2022-11-10 17:46:01 +0900

    add per_record graph

--- src/graph.py
+++ src/graph.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os.path as os
 
-elemNum = 30
+ELEMNUM = 30
 
 plt.figure(figsize=(15,10), tight_layout=True)
 plt.rcParams.update({
@@ -14,54 +14,73 @@ plt.rcParams.update({
 		"lines.linewidth": 4
 })
 
+def bar(xList, yList, yLabel, title, saveName):
+	plt.bar([i for i in range(len(xList))], yList)
+	plt.xticks([i for i in range(len(xList))], xList, rotation=90)
+	plt.xlabel("function")
+	plt.ylabel(yLabel)
+	plt.title(title)
+	plt.savefig(saveName)
+	plt.clf()
+
+def sort(xList, yList):
+	oy = yList
+	ox = xList
+	yList.sort(reverse=True)
+# slice
+	if ELEMNUM < len(xList):
+		yList = yList[:ELEMNUM]
+		xList = ELEMNUM*[0]
+# sort
+	for yi, y in enumerate(yList):
+		oi = oy.index(y)
+		xList[yi] = ox[oi]
+	return xList, yList
+
 def draw(foldername, filename, yLabel):
 	xList = [] # x value (function name) list of graph
 	yList = [] # y value list of graph
-	yRatioList = [] # y ratio value list of graph
+	xListOld = []
+	yListOld = []
+	yListPerRec = [] # y value per record
 	recordNum = 0
-	# read file 
+# read file 
 	fx = open("../record/" + foldername + "/" + filename, "r")
 	while True:
 		line = fx.readline()
 		t = line.split(" ")
 		if t[0] == "#" or not line:
 			if recordNum > 0:
-				# sort
-				oy = yList
-				ox = xList
-				yList.sort(reverse=True)
-				if elemNum < len(xList):
-					yList = yList[:elemNum]
-					xList = elemNum*[0]
-				for yi, y in enumerate(yList):
-					oi = oy.index(y)
-					xList[yi] = ox[oi]
-				# get ratio of y
-				sum = np.sum(yList)
-				yRatioList = [i/sum for i in yList]
-				# graph
-				plt.bar([i for i in range(len(xList))], yList)
-				plt.xticks([i for i in range(len(xList))], xList, rotation=90)
-				plt.xlabel("function")
-				plt.ylabel(yLabel)
-				plt.title(yLabel + " of Functions")
-				plt.savefig("../record/" + foldername + "/" + str(recordNum)+"_"+filename + ".png")
-				plt.clf()
-				plt.bar([i for i in range(len(xList))], yRatioList)
-				plt.xticks([i for i in range(len(xList))], xList, rotation=90)
-				plt.xlabel("function")
-				plt.ylabel(yLabel)
-				plt.title(yLabel + " Ratio of Functions")
-				plt.savefig("../record/" + foldername + "/" + str(recordNum)+"_"+filename + "_ratio.png")
-				plt.clf()
+# get y or one record
+				for xi, x in enumerate(xList):
+					if x in xListOld:
+						oldXIdx = xListOld.index(x)
+						yListPerRec.append(yList[xi] - yListOld[oldXIdx])
+					else:
+						yListPerRec.append(yList[xi])
+				xListOld = xList
+				yListOld = yList
+# sort
+				xList, yList = sort(xList, yList)
+				xListPerRec, yListPerRec = sort(xListOld, yListPerRec)
+# get ratio of y
+				sumY = np.sum(yList)
+				sumYPerRec = np.sum(yListPerRec)
+				yRatioList = [i/sumY for i in yList]
+				yRatioListPerRec = [i/sumYPerRec for i in yListPerRec]
+# graph
+				bar(xList, yList, yLabel, yLabel + " of Functions", "../record/" + foldername + "/" + str(recordNum)+"_"+filename + ".png")
+				bar(xList, yListPerRec, yLabel, yLabel + " of Functions", "../record/" + foldername + "/" + str(recordNum)+"_"+filename + "_perRecord.png")
+				bar(xList, yRatioList, yLabel, yLabel + " Ratio of Functions", "../record/" + foldername + "/" + str(recordNum)+"_"+filename + "_ratio.png")
+				bar(xList, yRatioListPerRec, yLabel, yLabel + " Ratio of Functions", "../record/" + foldername + "/" + str(recordNum)+"_"+filename + "_ratio_perRecord.png")
+
 				xList = []
 				yList = []
+				yListPerRec = []
 		if not line:
 			break
 		if t[0] == '#':
 			recordNum+=1
-			xList = []
-			yList = []
 		else:
 			xList.append(t[0])
 			yList.append(int(t[1]))
--- src/interface.py
+++ src/interface.py
@@ -14,6 +14,7 @@ class App(QWidget):
 		self.folder_name = folder_name
 		self.rn = 1
 		self.ratio = False
+		self.per_rec = False
 		self.ctg = "cnt"
 # init
 		self.init_ui()
@@ -24,21 +25,27 @@ class App(QWidget):
 		self.display_center()
 		self.resize(1700, 1200)
 # ratio radio button
-		self.ratio_btn = QRadioButton("Show Ratio Instead", self)
+		self.ratio_btn = QCheckBox("Show Ratio Instead", self)
 		self.ratio_btn.clicked.connect(self.ratio_clk)
 		self.ratio_btn.resize(210, 40)
 		self.ratio_btn.move(1390, 20)
-		self.ratio_btn.setStyleSheet("QRadioButton{font-size: 17px;} QRadioButton::indicator{ width: 30px; height: 30px; };")
+		self.ratio_btn.setStyleSheet("QCheckBox{font-size: 17px;} QCheckBox::indicator{ width: 30px; height: 30px; };")
+# perRecord radio button
+		self.per_rec_btn = QCheckBox("Not Cumulative per Record", self)
+		self.per_rec_btn.clicked.connect(self.per_rec_clk)
+		self.per_rec_btn.resize(280, 40)
+		self.per_rec_btn.move(1105, 20)
+		self.per_rec_btn.setStyleSheet("QCheckBox{font-size: 17px;} QCheckBox::indicator{ width: 30px; height: 30px; };")
 # labels
 		ctg_label = QLabel(self)
 		ctg_label.setText("Category")
 		ctg_label.resize(100, 40)
-		ctg_label.move(100, 20)
+		ctg_label.move(100, 70)
 		ctg_label.setStyleSheet("font-size: 17px;")
 		rn_label = QLabel(self)
 		rn_label.setText("Record No.")
 		rn_label.resize(100, 40)
-		rn_label.move(100, 60)
+		rn_label.move(100, 125)
 		rn_label.setStyleSheet("font-size: 17px;")
 # get info
 		info_f = open("../record/" + self.folder_name + "/info.json", "r");
@@ -57,16 +64,19 @@ class App(QWidget):
 		new_btn = QPushButton(self.ctg_names[i], self)
 		new_btn.clicked.connect(lambda: self.category_btn_clk(self.ctgs[i]))
 		new_btn.resize(150, 40)
-		new_btn.move(155*i + 200, 20)
+		new_btn.move(155*i + 200, 70)
 		new_btn.setStyleSheet("background-color: white;" "border-color: black;" "font-size: 17px;")
 	def create_record_btn(self, rn):
 		new_btn = QPushButton(str(rn), self)
 		new_btn.clicked.connect(lambda: self.record_btn_clk(rn))
 		new_btn.resize(40, 40)
-		new_btn.move(45*rn + 160, 65)
+		new_btn.move(45*rn + 160, 125)
 		new_btn.setStyleSheet("background-color: white;" "border-color: black;" "font-size: 17px;")
 	def ratio_clk(self):
 		self.ratio = self.ratio_btn.isChecked()
+		self.append_img()
+	def per_rec_clk(self):
+		self.per_rec = self.per_rec_btn.isChecked()
 		self.append_img()
 	def category_btn_clk(self, ctg):
 		self.ctg = ctg
@@ -76,13 +86,16 @@ class App(QWidget):
 		self.append_img()
 	def append_img(self):
 		pic = QLabel(self)
+		png_name = "../record/" + self.folder_name + "/" + str(self.rn) + "_" + self.ctg
 		if self.ratio:
-			pic.setPixmap(QPixmap("../record/" + self.folder_name + "/" + str(self.rn) + "_" + self.ctg + "_ratio.png"))
-		else:	
-			pic.setPixmap(QPixmap("../record/" + self.folder_name + "/" + str(self.rn) + "_" + self.ctg + ".png"))
+			png_name = png_name + "_ratio"
+		if self.per_rec:
+			png_name = png_name + "_perRecord"
+		png_name = png_name + ".png"
+		pic.setPixmap(QPixmap(png_name))
 		pic.show()
 		pic.resize(1500, 1000)
-		pic.move(100, 130)
+		pic.move(100, 210)
 	def display_center(self):
 		qr = self.frameGeometry()
 		cp = QDesktopWidget().availableGeometry().center()
